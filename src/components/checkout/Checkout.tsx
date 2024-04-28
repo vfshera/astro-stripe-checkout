829d755b58494ff6f6e01be17a375a5033a5af34
Franklin Shera <fshera96@gmail.com>
2024-04-28 04:15:55 +0300

    add checkout page

---
+++ src/components/checkout/Checkout.tsx
@@ -0,0 +1,27 @@
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+import { useCallback } from "react";
+
+const stripePromise = loadStripe(import.meta.env.PUBLIC_STRIPE_API_KEY!);
+
+export default function Checkout() {
+  const fetchClientSecret = useCallback(() => {
+    return fetch("/api/stripe/create-checkout-session", {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => data.clientSecret);
+  }, []);
+
+  return (
+    <EmbeddedCheckoutProvider
+      stripe={stripePromise}
+      options={{ fetchClientSecret }}
+    >
+      <EmbeddedCheckout />
+    </EmbeddedCheckoutProvider>
+  );
+}
