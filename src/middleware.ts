282f01d10a83fc9944d0ad01d57abbf596ce8e20
Franklin Shera <fshera96@gmail.com>
2024-04-28 10:58:32 +0300

    add simple csrf

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { defineMiddleware } from "astro:middleware";
+import { verifyRequestOrigin } from "oslo/request";
+
+export const onRequest = defineMiddleware(({ request }, next) => {
+  /**
+   * Allow requests from the same origin
+   */
+  if (new URL(request.url).pathname.startsWith("/api")) {
+    const originHeader = request.headers.get("Origin");
+    const hostHeader = request.headers.get("Host");
+
+    if (
+      !originHeader ||
+      !hostHeader ||
+      !verifyRequestOrigin(originHeader, [hostHeader])
+    ) {
+      return new Response(null, { status: 403 });
+    }
+  }
+
+  return next();
+});
