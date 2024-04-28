e32d3ba7ebe365d500fbc80bbce453ff7ac17112
Franklin Shera <fshera96@gmail.com>
2024-04-28 04:15:26 +0300

    add create session route

---
+++ src/pages/api/stripe/create-checkout-session.ts
@@ -0,0 +1,28 @@
+import { stripe } from "@/lib/stripe";
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request, site }) => {
+  const baseUrl = site || new URL(request.url).origin;
+  const return_url = new URL(
+    `/checkout/confirm?session_id={CHECKOUT_SESSION_ID}`,
+    baseUrl
+  );
+
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: "embedded",
+    line_items: [
+      {
+        price: "price_1MkfFaC2E9PG9OjQBxEmv6VU",
+        quantity: 2,
+      },
+      {
+        price: "price_1MS2qqC2E9PG9OjQg4dszKGy",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    return_url: return_url.toString(),
+  });
+
+  return new Response(JSON.stringify({ clientSecret: session.client_secret }));
+};
