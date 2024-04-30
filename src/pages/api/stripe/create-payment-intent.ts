8293f1cc6541ac689e6ccafac8d1d0782a50554a
Franklin Shera <fshera96@gmail.com>
2024-04-30 04:19:48 +0300

    add intent update

--- src/pages/api/stripe/create-payment-intent.ts
+++ src/pages/api/stripe/create-payment-intent.ts
@@ -18,3 +18,54 @@ export const POST: APIRoute = async ({ request }) => {
     return new Response(JSON.stringify({ error: "" }), { status: 400 });
   }
 };
+
+export const PUT: APIRoute = async ({ request }) => {
+  if (request.headers.get("Content-Type") !== "application/json") {
+    return new Response(JSON.stringify({ error: "Invalit request" }), {
+      status: 400,
+    });
+  }
+
+  try {
+    const body: { variantId?: string; intentId?: string } =
+      await request.json();
+
+    const result = updateSchema.safeParse(body);
+
+    if (result.error) {
+      return new Response(JSON.stringify({ error: "Invalid input!" }), {
+        status: 400,
+      });
+    }
+
+    const { variantId, intentId } = result.data;
+
+    const variant = product.variants.find((v) => v.variant_id === variantId);
+
+    if (!variant) {
+      return new Response(JSON.stringify({ error: "Invalid variant!" }), {
+        status: 400,
+      });
+    }
+    const PRICE = Math.round(parseFloat(variant.price) * 100 * variant.items);
+
+    await stripe.paymentIntents.update(intentId, {
+      amount: PRICE,
+      metadata: {
+        variant_id: variant.variant_id,
+      },
+    });
+
+    return new Response(JSON.stringify({ message: "Variant selected!" }), {
+      status: 200,
+    });
+  } catch (e) {
+    let message = "Something went wrong!";
+
+    if (e instanceof Error) {
+      message = e.message;
+    }
+
+    return new Response(JSON.stringify({ error: message }), { status: 400 });
+  }
+};
